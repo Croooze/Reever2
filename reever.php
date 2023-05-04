1a2bbe35081e38e994e2faadebc3320e9a1bf422
Croooze <95697387+Croooze@users.noreply.github.com>
2023-05-04 10:16:58 +0200

    Reever 1.0

---
+++ reever.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Reever</title>
+    <link rel='stylesheet' href='style.css'>
+</head>
+
+<body>
+    <div class="gauche">
+        <h1>REEVER</h1>
+        <p>Le site de rencontre et de retrouvaille</p>
+    </div>
+
+
+
+    <div class="droite">
+        <div class="connexion">
+            <div class="box">
+                <form action="Accueil.html">
+                    <h1 class="formulaire">CONNEXION</h1>
+                    <input type="text" placeholder="Adresse mail">
+                    <input type="password" placeholder="Mot de passe">
+                    <input type="submit" value="Connexion">
+                    <a href="Inscription.html">
+                        <p class="compte"> Créer un compte !</p>
+                    </a>
+                </form>
+            </div>
+            </a>
+        </div>
+    </div>
+</body>
+
+</html>
