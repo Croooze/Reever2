fa8bb86f58361f5c689d3bc8826fa813f9becf73
Croooze <95697387+Croooze@users.noreply.github.com>
2023-05-04 10:10:27 +0200

    Reever

---
+++ Inscription.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inscription</title>
+    <link rel='stylesheet' href='style.css'>
+</head>
+
+<body>
+    <div class="gauche">
+        <h1>REEVER</h1>
+        <p>Le site de rencontre et de retrouvaille</p>
+    </div>
+    <div class="droite">
+        <div class="box">
+            <form action="Accueil.html">
+                <h1 class="formulaire">INSCRIPTION</h1>
+                <input type="text" placeholder="Nom">
+                <input type="text" placeholder="Prénom">
+                <input type="text" placeholder="Adresse mail">
+                <input type="password" placeholder="Mot de passe">
+                <input type="password" placeholder="Confirmer le mot de passe">
+                <input class="date" type="date" id="bday" name="bday">
+                <input type="submit" value="Inscription">
+            </form>
+        </div>
+    </div>
+</body>
+
+</html>
