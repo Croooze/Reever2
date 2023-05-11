8124243b559dced69e067f2ea90aae6d8372ae3f
Alexisnico03 <alexis.nicolas@ipilyon.net>
2023-05-11 11:50:13 +0200

    MAJ inscription

--- Inscription.php
+++ Inscription.php
@@ -48,12 +48,12 @@ if (isset($_POST['envoyer'])) {
     </div>
     <div class="droite">
         <div class="box">
-            <form method="post" action="Accueil.php">
+            <form method="post" action="">
                 <h1 class="formulaire">INSCRIPTION</h1>
-                <input type="text" id="nom" placeholder="Nom">
-                <input type="text" id="prenom" placeholder="Prénom">
-                <input type="text" id="email" placeholder="Adresse mail">
-                <input type="password" id="mdp" placeholder="Mot de passe">
+                <input type="text" id="nom" name="nom" placeholder="Nom">
+                <input type="text" id="prenom" name="prenom" placeholder="Prénom">
+                <input type="text" id="email" name="email" placeholder="Adresse mail">
+                <input type="password" id="mdp" name="mdp" placeholder="Mot de passe">
                 <input type="password" id="mdp" placeholder="Confirmer le mot de passe">
                 <input class="date" type="date" id="bday" name="bday">
                 <input type="submit" value="Inscription" name="envoyer">
@@ -62,37 +62,5 @@ if (isset($_POST['envoyer'])) {
     </div>
 </body>
 
-<?php
-// Configuration de la base de données
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "reever";
-
-// Connexion à la base de données
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Vérification de la connexion
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
-// Récupération des données du formulaire
-$nom = $_POST['nom'];
-$prenom = $_POST['prenom'];
-$email = $_POST['email'];
-$mdp = $_POST['mdp'];
-$bday = $_POST['bday'];
-
-// Insertion des données dans la base de données
-$sql = "INSERT INTO formulaire (nom, prenom, email, mdp, bday) VALUES ('$nom', '$prenom', '$email', '$mdp', '$bday')";
-
-if ($conn->query($sql) === TRUE) {
-    echo "Les données ont été enregistrées avec succès.";
-} else {
-    echo "Erreur: " . $sql . "<br>" . $conn->error;
-}
-
-$conn->close();?>
-
 </html>
+
