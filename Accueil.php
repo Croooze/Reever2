14fa508aa6c50aee513dcd2acf6b1cd2702ed0d7
Croooze <95697387+Croooze@users.noreply.github.com>
2023-05-31 22:50:02 +0200

    modif création_event

--- Accueil.php
+++ Accueil.php
@@ -27,7 +27,7 @@ try {
         $userId = $_SESSION['user_id'];
 
         // Récupérer le dernier événement créé par l'utilisateur connecté
-        $sql = "SELECT e.nom FROM event e INNER JOIN liste l ON e.id_event = l.id_event WHERE l.id_user = :user_id ORDER BY e.id_event DESC LIMIT 1";
+        $sql = "SELECT e.nom, e.photo FROM event e INNER JOIN liste l ON e.id_event = l.id_event WHERE l.id_user = :user_id ORDER BY e.id_event DESC LIMIT 1";
         $stmt = $pdo->prepare($sql);
         $stmt->bindParam(':user_id', $userId);
         $stmt->execute();
@@ -49,17 +49,29 @@ try {
     </header>
 
     <section class="evenement">
-        <div class="content">
-            <?php if ($event) { ?>
-                <?php
-                $eventLink = "liste.php?nom=" . urlencode($event['nom']);
-                ?>
-<a href="<?php echo $eventLink; ?>" class="button"><?php echo $event['nom']; ?></a>
-            <?php } else { ?>
-                <p>Scannez ou Créez un événement pour voir la liste ici</p>
-            <?php } ?>
-        </div>
+        <?php if ($event) { ?>
+            <?php
+            $eventLink = "liste.php?nom=" . urlencode($event['nom']);
+            ?>
+            <a href="<?php echo $eventLink; ?>" class="event-link">
+                <div class="content">
+                    <?php if ($event['photo']) { ?>
+                        <img src="data:image/jpeg;base64,<?php echo base64_encode($event['photo']); ?>" alt="Event Photo" />
+                    <?php } ?>
+                    <span class="event-name <?php echo ($event['photo']) ? 'with-photo' : 'without-photo'; ?>">
+                        <?php echo ucfirst(strtolower($event['nom'])); ?>
+                    </span>
+                </div>
+            </a>
+        <?php } else { ?>
+            <div class="content">
+                <span class="event-name without-photo">
+                    Scannez ou Créez un événement pour voir la liste ici
+                </span>
+            </div>
+        <?php } ?>
     </section>
+
 
     <section class="card">
         <div class="left">
@@ -74,6 +86,43 @@ try {
             </b>
         </div>
     </section>
+
+    <script type="text/javascript">
+        function generateur(qr_texte) {
+            var qrcode = document.querySelector("#qrcode");
+            qrcode.innerHTML = "";
+            qrcode.style.display = "flex";
+            new QRCode(qrcode, qr_texte);
+        }
+
+        document.querySelector("button[type='submit']").addEventListener("click", function(event) {
+            event.preventDefault();
+            var nom = document.querySelector("input[name='nom']").value;
+            if (nom !== "") {
+                var qrcode = document.querySelector("#qrcode");
+                qrcode.style.display = "none";
+                var url = "http://localhost/reever2/liste.php?nom=" + encodeURIComponent(nom);
+                generateur(url);
+                if (document.querySelector(".center")) {
+                    document.querySelector(".center").style.display = "block";
+                } else {
+                    var centerDiv = document.createElement("div");
+                    centerDiv.className = "center";
+                    var customBtn = document.createElement("a");
+                    customBtn.href = "personnalisation.php";
+                    customBtn.className = "custom-btn";
+                    customBtn.textContent = "Personnaliser Événement";
+                    centerDiv.appendChild(customBtn);
+                    document.body.appendChild(centerDiv);
+                }
+                // Mettre à jour le lien de l'événement sur la page d'accueil
+                var eventLink = document.querySelector(".event-link");
+                if (eventLink) {
+                    eventLink.href = "liste.php?nom=" + encodeURIComponent(nom);
+                }
+            }
+        });
+    </script>
 
 </body>
 
--- Création_event.php
+++ Création_event.php
@@ -26,21 +26,12 @@
             <form action="" method="POST">
                 <h1>ÉVÉNEMENT</h1>
                 <input type="text" name="nom" placeholder="Nom de l'événement">
-                <button type="submit">Générer le QR code</button>
+                <button type="submit" name="submit">Générer le QR code</button>
             </form>
         </div>
 
         <div class="qrcode" id="qrcode"></div>
     </section>
-
-    <script src="qrcode.min.js"></script>
-    <script type="text/javascript">
-        function generateur(qr_texte) {
-            var qrcode = document.querySelector("#qrcode");
-            qrcode.style.display = "flex";
-            new QRCode(qrcode, qr_texte);
-        }
-    </script>
 
     <?php
     session_start();
@@ -51,17 +42,15 @@
 
     try {
         $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     } catch (PDOException $e) {
         echo "La connexion a échoué: " . $e->getMessage();
     }
 
-    if (isset($_POST['nom'])) {
-        $nom = $_POST['nom'];
-
-        // Vérifier si l'utilisateur est connecté
+    if (isset($_POST['submit'])) {
         if (isset($_SESSION['user_id'])) {
             $userId = $_SESSION['user_id'];
+            $nom = $_POST['nom'];
 
             $sql = "INSERT INTO event(nom) VALUES (:nom)";
             $stmt = $conn->prepare($sql);
@@ -70,7 +59,6 @@ $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
             $eventId = $conn->lastInsertId();
 
-            // Enregistrer l'événement dans la liste de l'utilisateur connecté
             $sql = "INSERT INTO liste(id_event, id_user) VALUES (:event_id, :user_id)";
             $stmt = $conn->prepare($sql);
             $stmt->bindParam(':event_id', $eventId);
@@ -78,10 +66,31 @@ $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $stmt->execute();
 
             $url = "http://localhost/reever2/liste.php?nom=" . urlencode($nom);
-            echo "<script>generateur('$url')</script>";
+            echo "<script>generateur('$url');</script>";
         }
+    } elseif (isset($_GET['nom'])) {
+        echo '<div class="center"><a href="personnalisation.php" class="custom-btn">Personnaliser Événement</a></div>';
     }
     ?>
+
+    <script type="text/javascript">
+        function generateur(qr_texte) {
+            var qrcode = document.querySelector("#qrcode");
+            qrcode.style.display = "flex";
+            qrcode.innerHTML = "";
+            new QRCode(qrcode, qr_texte);
+        }
+
+        document.querySelector("button[name='submit']").addEventListener("click", function (event) {
+            event.preventDefault();
+            var nom = document.querySelector("input[name='nom']").value;
+            if (nom.trim() !== "") {
+                document.querySelector("form").submit();
+            } else {
+                alert("Veuillez saisir un nom d'événement");
+            }
+        });
+    </script>
 
 </body>
 
