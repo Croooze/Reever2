3d36a940d087fb3e187db0aafeef26935ea3878f
Alexisnico03 <alexis.nicolas@ipilyon.net>
2023-06-24 15:14:01 +0200

    pp header fonctionnel page template

--- Profil_template.php
+++ Profil_template.php
@@ -14,13 +14,30 @@ try {
     $errors[] = "La connexion a échoué : " . $e->getMessage();
 }
 
+// Récupérer les informations de l'utilisateur connecté
 if (isset($_SESSION['user_id'])) {
     $userId = $_SESSION['user_id'];
 
-    // Effectuer une requête pour récupérer les informations de l'utilisateur
+    // Effectuer une requête pour récupérer les informations de l'utilisateur connecté
+    $sql = "SELECT nom, prenom, description, photo, instagram FROM user WHERE id_user = :userId";
+    $stmt = $conn->prepare($sql);
+    $stmt->bindParam(':userId', $userId);
+    $stmt->execute();
+    $userConnecte = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    if ($userConnecte) {
+        $photoUtilisateurConnecte = $userConnecte['photo'];
+    }
+}
+
+// Récupérer les informations de la personne sur laquelle vous avez cliqué
+if (isset($_GET['nom'])) {
+    $nomParticipant = $_GET['nom'];
+
+    // Effectuer une requête pour récupérer les informations de la personne sur laquelle vous avez cliqué
     $sql = "SELECT nom, prenom, description, photo, instagram FROM user WHERE nom = :nomParticipant";
     $stmt = $conn->prepare($sql);
-    $stmt->bindParam(':nomParticipant', $_GET['nom']);
+    $stmt->bindParam(':nomParticipant', $nomParticipant);
     $stmt->execute();
     $user = $stmt->fetch(PDO::FETCH_ASSOC);
 
@@ -32,11 +49,7 @@ if (isset($_SESSION['user_id'])) {
         $instaUtilisateur = $user['instagram'];
     }
 }
-
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
 
 <!DOCTYPE html>
 <html lang="en">
@@ -50,22 +63,23 @@ if (isset($_SESSION['user_id'])) {
 </head>
 
 <body>
-<header>
+    <header>
         <a href="Accueil.php" class="logo">REEVER</a>
         <nav>
-        <a href="Profil.php">Profil</a>
+            <a href="Profil.php">Profil</a>
             <a href="Paramètre.php">Paramètres</a>
         </nav>
-        <?php if ($user && $user['photo']) { ?>
-                <a href="Profil.php" class="profile-link">
-                    <img src="data:image/jpeg;base64,<?php echo base64_encode($user['photo']); ?>" alt="Photo de profil" class="profile-photo">
-                </a>
-            <?php } else { ?>
-                <a href="Profil.php" class="profile-link">
-                    <img src="img/default.png" alt="Photo de profil" class="profile-photo">
-                </a>
-            <?php } ?>
+        <?php if ($_SESSION['user_id'] && $photoUtilisateurConnecte) { ?>
+            <a href="Profil.php" class="profile-link">
+                <img src="data:image/jpeg;base64,<?php echo base64_encode($photoUtilisateurConnecte); ?>" alt="Photo de profil" class="profile-photo">
+            </a>
+        <?php } else { ?>
+            <a href="Profil.php" class="profile-link">
+                <img src="img/default.png" alt="Photo de profil" class="profile-photo">
+            </a>
+        <?php } ?>
     </header>
+
     <div class="container2">
         <div class="centered-element">
             <?php if ($user && $photoUtilisateur) { ?>
@@ -78,11 +92,10 @@ if (isset($_SESSION['user_id'])) {
             </div>
         </div>
         <div class="info">
-            <p class="description"><?php echo($user['description']); ?></p>
+            <p class="description"><?php echo ($user['description']); ?></p>
             <p class="instagram"><a href="https://www.instagram.com/<?php echo ($user['instagram']); ?>" target="_blank">@<?php echo ($user['instagram']); ?></a></p>
             <div class="detail">
-            <a href="javascript:history.back()">Retour</a>
-
+                <a href="javascript:history.back()">Retour</a>
             </div>
         </div>
     </div>
