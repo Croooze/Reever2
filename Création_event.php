1538edfe210b155ed6b3a338f1b5f11f25a49237
Croooze <95697387+Croooze@users.noreply.github.com>
2023-06-27 16:52:38 +0200

    Modif_scrollbarre

--- Création_event.php
+++ Création_event.php
@@ -56,6 +56,21 @@ if (isset($_POST['submit'])) {
             $stmt->execute();
 
             $eventId = $conn->lastInsertId();
+
+            // Vérifier si un fichier image a été sélectionné
+            if (isset($_FILES['photo']) && $_FILES['photo']['error'] === UPLOAD_ERR_OK) {
+                $photo = $_FILES['photo']['tmp_name'];
+
+                // Lire le contenu du fichier photo
+                $photoContent = file_get_contents($photo);
+
+                // Mettre à jour le champ 'photo' dans la table 'event'
+                $sql = "UPDATE event SET photo = :photo WHERE id_event = :event_id";
+                $stmt = $conn->prepare($sql);
+                $stmt->bindParam(':photo', $photoContent, PDO::PARAM_LOB);
+                $stmt->bindParam(':event_id', $eventId);
+                $stmt->execute();
+            }
         }
     }
 }
@@ -69,7 +84,13 @@ if (isset($_POST['submit'])) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-    <link rel="shortcut icon" type="img/png" href="img/favicon.ico"/>
+    <link rel="shortcut icon" type="img/png" href="img/favicon.ico" />
+    <style>
+        .zone button[type="file"] {
+            display: block;
+            margin-top: 10px;
+        }
+    </style>
     <title>Création événement</title>
 </head>
 
@@ -81,21 +102,23 @@ if (isset($_POST['submit'])) {
             <a href="Paramètre.php">Paramètres</a>
         </nav>
         <?php if ($user && $user['photo']) { ?>
-                <a href="Profil.php" class="profile-link">
-                    <img src="data:image/jpeg;base64,<?php echo base64_encode($user['photo']); ?>" alt="Photo de profil" class="profile-photo">
-                </a>
-            <?php } else { ?>
-                <a href="Profil.php" class="profile-link">
-                    <img src="img/default.png" alt="Photo de profil" class="profile-photo">
-                </a>
-            <?php } ?>
+            <a href="Profil.php" class="profile-link">
+                <img src="data:image/jpeg;base64,<?php echo base64_encode($user['photo']); ?>" alt="Photo de profil"
+                    class="profile-photo">
+            </a>
+        <?php } else { ?>
+            <a href="Profil.php" class="profile-link">
+                <img src="img/default.png" alt="Photo de profil" class="profile-photo">
+            </a>
+        <?php } ?>
     </header>
     <section class="zone">
         <div class="donnée">
-            <form action="" method="POST">
+            <form action="" method="POST" enctype="multipart/form-data">
                 <h1>ÉVÉNEMENT</h1>
                 <input type="text" name="nom" placeholder="Nom de l'événement">
                 <button type="submit" name="submit">Générer le QR code</button>
+                <input type="file" name="photo" accept="image/*">
             </form>
         </div>
 
